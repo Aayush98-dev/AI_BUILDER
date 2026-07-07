9be5200db8b5bb357154d0feab79246e13b667f5
Ayush Singh <singhaayushrajpoot4567@gmail.com>
2026-07-07 21:49:57 +0530

    commit

--- server/configs/openai.ts
+++ server/configs/openai.ts
@@ -1,13 +1,13 @@
-// import OpenAI from 'openai';
+// import OpenAI = require("openai");
 
 // const openai = new OpenAI({
-//   apiKey: process.env.AI_API_KEY!,
+//   apiKey: process.env.AI_API_KEY,
 //   baseURL: "https://openrouter.ai/api/v1"
 // });
 
 // export default openai;
 
-import OpenAI from "openai";
+import OpenAI = require("openai");
 
 const openai = new OpenAI({
   apiKey: process.env.AI_API_KEY,
